f4a7ff472e263e8604bc9c697838a418cd1e419b
masnoble <jnthomas522@gmail.com>
2022-05-02 15:08:19 -0400

    Add Drag n Drop from courses and reqs

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {cloneElement, Component} from 'react';
 
 class Course extends Component {
 	
@@ -6,17 +6,41 @@ class Course extends Component {
         
 		let dispYear;
 		const courseList = this.props.data;
-        //console.log(Object.entries(courseList));
+        // console.log(Object.entries(courseList));
 		let courses = Object.entries(courseList).map(([key,value]) => {
 			let courseName = ""
+			let courseID = "";
 			if(typeof this.props.catalog.courses[value] == "undefined"){
 				courseName = value+ " " + "No Name Found";
 			}
 			else{
 				courseName = value+ " " + this.props.catalog.courses[value].name;
+				courseID = value;
 			}
+
+			const dragStart = e => {
+				var target = e.target;
+				if (e.target.parentNode.className == "panel") {
+					target = target.cloneNode()
+				}
+				e.dataTransfer.setData("course_id", target.id);
+
+				setTimeout(() => {
+					target.style.display = "none";
+				}, 0);
+			}
+
+			const dragOver = e => {
+				e.stopPropagation();
+			}
+
 		  	return (
-				<p>
+				<p
+					id={courseID}
+					draggable='true'
+					onDragStart={dragStart}
+					onDragOver={dragOver}
+				>
 					{courseName}
 				</p>
 			);	
--- src/components/Term.js
+++ src/components/Term.js
@@ -4,6 +4,7 @@ import Course from './Course';
 class Term extends Component {
 	
 	render (){
+		
 		let dispYear;
 		if(this.props.term != "Fall"){
 			dispYear = this.props.year+1;
@@ -12,8 +13,28 @@ class Term extends Component {
 			dispYear = this.props.year;
 		}
 		//console.log(Object.keys(this.props.data));
+
+		const drop = e => {
+			e.preventDefault();
+			const course_id = e.dataTransfer.getData('course_id');
+
+			const c = document.getElementById(course_id);
+			c.style.display = 'block';
+
+			e.target.appendChild(c);
+		}
+
+		const dragOver = e => {
+			e.preventDefault();
+		}
+		
 		return (
-		<div className="semester">
+		<div 
+			id={this.props.term + dispYear}
+			className={"semester " + this.props.term + dispYear}
+			onDrop={drop}
+			onDragOver={dragOver}
+			>
 			{this.props.term} {dispYear}
 			<Course data={Object.keys(this.props.data)} catalog={this.props.catalog}/>
 		</div>
