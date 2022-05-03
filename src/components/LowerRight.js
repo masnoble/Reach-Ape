9dda163c62b1d3e0183e5866572ad4396ca11220
masnoble <jnthomas522@gmail.com>
2022-05-03 12:07:58 -0400

    Drag and Drop with catalot

--- src/components/LowerRight.js
+++ src/components/LowerRight.js
@@ -11,47 +11,53 @@ class LowerRight extends Component {
 
     componentDidMount() {
 
+        document.addEventListener("dragstart", function (event) {
+            // store a ref. on the dragged elem
+            var target = event.target.cloneNode();
 
-        console.log(this.props.catalog);
+            event.dataTransfer.setData("course_id", target.id);
+
+            setTimeout(() => {
+                target.style.display = "none";
+            }, 0);
+            
+        }, false);
+    
+
+
+         /* events fired on the drop targets */
+        document.addEventListener("dragover", function (event) {
+            // prevent default to allow drop
+            event.preventDefault();
+        }, false);
         
 
         if(this.props.catalog){
-            // this.props.build();
             let courses = Object.values(this.props.catalog.courses);
 		    $("#catalog").DataTable({
                 retrieve: true,
                 data: courses, 
+                'columnDefs': [
+                    {
+                       'targets': 0,
+                       'createdCell':  function (td, cellData, rowData, row, col) {
+                          $(td).attr('class', 'tbl'); 
+                          $(td).attr('draggable', 'true'); 
+                          $(td).attr('id', cellData);
+                       }
+                    }
+                 ],
                 columns: [
                     { data: 'id', title: 'ID' },
                     { data: 'name', title: 'Name'},
                     { data: 'description',title: 'Description' },
                     { data: 'credits',title: 'Credits' }
-                    ]
-                });
-            }
+                ]
+            });
+        }
         
     }  
 
-    build(){
-
-        console.log("THISN IS MY NAME");
-
-        if(this.props.catalog){
-            // this.props.build();
-            let courses = Object.values(this.props.catalog.courses);
-		    $("#catalog").DataTable({
-			data: courses, 
-			columns: [
-				{ data: 'id', title: 'ID' },
-				{ data: 'name', title: 'Name'},
-				{ data: 'description',title: 'Description' },
-				{ data: 'credits',title: 'Credits' }
-                ]
-            });
-
-
-        }
-    }
     componentWillUnmount(){
        $('.data-table-wrapper')
        .find('table')
@@ -73,7 +79,6 @@ class LowerRight extends Component {
         
     }
 }
-
 
 
 export default LowerRight;
